8ff107a3dfd00e0c71774f3f0bde7b7cef8855dd
VaigaiCart <vaigaicart@gmail.com>
2024-08-08 10:07:57 +0530

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -43,7 +43,7 @@ const productSchema = new mongoose.Schema({
                 'Meats',
                 'Spices Masalas',
                 'House Holds',
-                'Dals'
+                'Dals Nuts'
             ],
             message : "Please select correct category"
         }
